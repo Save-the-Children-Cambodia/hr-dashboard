7e9fcf8a0f4e3a38c2c8d45e4bfa7a5a5e1f41c0
DuchPanhathun <duch.panhathun.2821@rupp.edu.kh>
2024-12-28 19:54:23 +0700

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
     <div className="min-h-screen bg-gray-100">
       {/* Navigation Bar */}
       <nav className="bg-white shadow-lg">
-        <div className="max-w-7xl mx-auto">
+        <div className="mx-auto pl-5 pr-5">
           <div className="flex justify-between h-20">
             <div className="flex items-center">
               <span className="text-xl  font-semibold text-gray-800">HR Dashboard</span>
